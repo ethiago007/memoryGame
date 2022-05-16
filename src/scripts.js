9729a436c7383271514ad8dd78404008e262c5ca
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2022-05-16 11:26:18 +0100

    added logic for three more cards

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 let akashi = new Card("./../img/akashi.jpg");
 let akatsuki = new Card("./../img/akatsuki.jpg")
 let aomine = new Card('./../img/aomine.jpg');
@@ -68,4 +69,173 @@ $('#card11').click(function() {
       // });
     }
   }
+});
+$('#card2').click(function() {
+    counter++;
+    $('#card2Src').attr('src', akatsuki.cardFace);
+    if (guess1 === 0) {
+      guess1 = akatsuki.cardFace;
+      guess1id = '#card2Src';
+    } else if (guess1 !== 0) {
+      guess2 = akatsuki.cardFace;
+      guess2id = '#card2Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
+  $('#card44').click(function() {
+    counter++;
+    $('#card12Src').attr('src', akatsuki.cardFace);
+    if (guess1 === 0) {
+      guess1 = akatsuki.cardFace;
+      guess1id = '#card12Src';
+    } else if (guess1 !== 0) {
+      guess2 = akatsuki.cardFace;
+      guess2id = '#card12Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
+  $('#card3').click(function() {
+    counter++;
+    $('#card3Src').attr('src', aomine.cardFace);
+    if (guess1 === 0) {
+      guess1 = aomine.cardFace;
+      guess1id = '#card3Src';
+    } else if (guess1 !== 0) {
+      guess2 = aomine.cardFace;
+      guess2id = '#card3Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
+  $('#card55').click(function() {
+    counter++;
+    $('#card13Src').attr('src', aomine.cardFace);
+    if (guess1 === 0) {
+      guess1 = aomine.cardFace;
+      guess1id = '#card13Src';
+    } else if (guess1 !== 0) {
+      guess2 = aomine.cardFace;
+      guess2id = '#card13Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
+  $('#card4').click(function() {
+    counter++;
+    $('#card4Src').attr('src', eren.cardFace);
+    if (guess1 === 0) {
+      guess1 = eren.cardFace;
+      guess1id = '#card4Src';
+    } else if (guess1 !== 0) {
+      guess2 = eren.cardFace;
+      guess2id = '#card4Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
+  $('#card77').click(function() {
+    counter++;
+    $('#card15Src').attr('src', eren.cardFace);
+    if (guess1 === 0) {
+      guess1 = eren.cardFace;
+      guess1id = '#card15Src';
+    } else if (guess1 !== 0) {
+      guess2 = eren.cardFace;
+      guess2id = '#card15Src';
+    }
+    if (guess2 !== 0) {
+      if (guess1 === guess2) {
+        guess1 = 0;
+        guess2 = 0;
+        
+      }
+      else {
+        guess1 = 0;
+        guess2 = 0;
+        // $('#response').html("<span id='wrong'><h3>You guessed incorrect. Click here to try again.</h3></span>");
+        // $('#wrong').click(function() {
+          $('#response').html("");
+          $(guess1id).attr('src', '');
+          $(guess2id).attr('src', '');
+        // });
+      }
+    }
+  });
 });
